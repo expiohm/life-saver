c648913a05e0fc64644ea7587dae1013b6de32ef
Dheeraj <dheeraj@expiohm.com>
2022-12-13 03:31:33 +0530

    reduce timeout

--- apps/life-saver/pages/api/test.tsx
+++ apps/life-saver/pages/api/test.tsx
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
-    // wait for 10 sec to simulate a slow API
+    // wait for 2 sec to simulate a slow API
     setTimeout(() => {
         res.status(200).json({ name: 'John Doe' })
-    }, 10000)
+    }, 2000)
   }
